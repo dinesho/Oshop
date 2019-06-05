787bb9421ec87416c69f4caa7ace1db6e9c2caed
Dinesh Don <dinesh.opatha@gmail.com>
2019-06-05 15:05:13 -0400

    update get category method to include keys

--- src/app/category.service.ts
+++ src/app/category.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from "@angular/core";
 import { AngularFireDatabase } from "@angular/fire/database";
+import { map } from "rxjs/operators";
 
 @Injectable({
   providedIn: "root"
@@ -8,6 +9,25 @@ export class CategoryService {
   constructor(private db: AngularFireDatabase) {}
 
   getCategories() {
-    return this.db.list("categories").valueChanges();
+    return this.db
+      .list("categories")
+      .snapshotChanges()
+      .pipe(
+        map(changes =>
+          changes.map(c => ({ key: c.payload.key, ...c.payload.val() }))
+        )
+      );
   }
+
+  //   constructor(db: AngularFireDatabase) {
+  //     this.itemsRef = db.list("messages");
+  //     // Use snapshotChanges().map() to store the key
+  //     this.items = this.itemsRef
+  //       .snapshotChanges()
+  //       .pipe(
+  //         map(changes =>
+  //           changes.map(c => ({ key: c.payload.key, ...c.payload.val() }))
+  //         )
+  //       );
+  //   }
 }
