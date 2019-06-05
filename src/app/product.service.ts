b3ff3ec849bbe2ff2684a0d1ae0755203e38f342
Dinesh Don <dinesh.opatha@gmail.com>
2019-06-05 18:55:04 -0400

    add product listing feature

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from "@angular/core";
 import { AngularFireDatabase } from "@angular/fire/database";
+import { map } from "rxjs/operators";
 
 @Injectable({
   providedIn: "root"
@@ -9,5 +10,16 @@ export class ProductService {
 
   create(product) {
     this.db.list("products").push(product);
+  }
+
+  getAll() {
+    return this.db
+      .list("products")
+      .snapshotChanges()
+      .pipe(
+        map(changes =>
+          changes.map(c => ({ key: c.payload.key, ...c.payload.val() }))
+        )
+      );
   }
 }
